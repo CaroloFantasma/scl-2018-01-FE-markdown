8b6cc9b372d3c1cc7655c357ca8f7513d68064d6
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-23 17:49:31 -0300

    arreglando errores

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+#!/usr/bin/env node
+
 let fs = require('fs');
 let path = require('path');
+const mdLinks = require('./lib/md-links');
 
-let pathSupplied = process.argv[2];
-let extensionFilter = md;
-
-function extension(element) {
-  let extName = path.extname(element);
-  return extName === '.' + extensionFilter; // busca esa extensión del archivo
-};
-
-fs.readdir(pathSupplied, function(err, list) {
-  list.filter(extension).forEach(function(value) {
-    console.log(value);
-  });
+let dirPath = process.argv[2];
+let dirBuf = Buffer.from(dirPath);
+let fileType = '.' + process.argv[3]; // Extensión del archivo
+let files = [];
+fs.readdir(dirBuf, function(err, list) {
+  if (err) throw err;
+  for (let i = 0; i < list.length; i++) {
+    if (path.extname(list[i]) === fileType) {
+      console.log(list[i]); // imprime el archivo
+      files.push(list[i]); // almacena el nombre del archivo dentro de un arreglo de archivos
+    }
+  }
 });
-
