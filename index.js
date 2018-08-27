b7f085c70ad52e13556a737103f43f6e9559f1d3
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-27 10:38:32 -0300

    Se imprime la ruta, nombre del archivo y su contenido

--- index.js
+++ index.js
@@ -12,21 +12,16 @@ let cwdToString = Buffer.from(currentDirectory);
 
 //  Lee los contenidos del directorio
 fs.readdir(cwdToString, (error, files) => {
-  files.forEach(file => {
-    console.log(file);
+  // Selecciona los archivos con extensión .md
+  for (let i in files) {
+    if (path.extname(files[i]) === '.md') {
 
-    // Selecciona los archivos con extensión .md
-    for (let i in files) {
-      if (path.extname(files[i]) === '.md') {
-        console.log(files[i]);
-
-        //  Leer contenido del archivo
-        fs.readFile(files[i], 'utf8', (err, data) => {
-          if (err) throw err;
-          console.log(data);
-        });
-      }
+      //  Leer contenido del archivo
+      fs.readFile(files[i], 'utf8', (err, data) => {
+        if (err) throw err;
+        console.log(data);
+      });
     }
-  });
+  }
 });
 
