931a68ff9967bb5b5cb28a67b5c1678061b1a12b
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-26 22:20:42 -0300

    Entrega directorio y archivos .md

--- index.js
+++ index.js
@@ -3,8 +3,25 @@
 let fs = require('fs');
 let path = require('path');
 
-// Leer archivo
-fs.readFile('README.md', 'utf8', (err, data) => { 
-  if (err) throw err;
-  console.log(data);
+//  Ruta actual del directorio (Current Working Directory)
+let currentDirectory = process.cwd();
+console.log(`Current working directory: ${process.cwd()}`);
+let cwdToString = Buffer.from(currentDirectory);
+
+fs.readdir(cwdToString, (error, files) => {
+  files.forEach(file => {
+    console.log(file);
+  });
 });
+
+// Leer archivo
+// fs.readFile(file, 'utf8', (err, data) => { 
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// // Selecciona los archivos con extensión .md
+// for (let i in files) {
+//   if (path.extname(files[i]) === '.md') {
+//   }
+// }
---
+++ md-examples/links.md
@@ -0,0 +1,7 @@
+Acá va un link:
+https://www.google.com/
+Aquí hay otro link:
+https://github.com/CaroloFantasma
+Y otro más:
+https://github.com/CaroFantasma
+Adiós!
