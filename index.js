e8a8f0c3919c11b4dc343d3a904b099015650614
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-27 21:43:31 -0300

    links, status, color

--- index.js
+++ index.js
@@ -1,43 +1,36 @@
 #!/usr/bin/env node
-
-let fs = require('fs');
-let path = require('path');
+const fs = require('fs');
+const path = require('path');
 const mdLinks = require('./lib/md-links');
 const Marked = require('marked');
+const fetch = require('node-fetch');
+const colors = require('colors');
 
 //  Ruta actual del directorio (Current Working Directory)
 let currentDirectory = process.cwd();
-console.log(`Current working directory: ${process.cwd()}`);
 let cwdToString = Buffer.from(currentDirectory);
+console.log(`Current working directory: ${process.cwd()}`.magenta);
 
 //  Lee los contenidos del directorio
 fs.readdir(cwdToString, (error, files) => {
   // Selecciona los archivos con extensión .md
-  for (let i in files) {
-    if (path.extname(files[i]) === '.md') {
-      console.log(files[i]);
-
-      //  Leer contenido del archivo
-      fs.readFile(files[i], 'utf8', (err, data) => {
-        if (err) throw err;
-        console.log(data);
+  files.forEach(file => {
+    if (path.extname(file).toLowerCase() === '.md') {
+      // Leer contenido del archivo
+      fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+          console.log(error);
+        } else {
+          mdLinks(data).forEach(element => {
+            console.log(file.cyan, element.href.green, element.text.blue);
+            fetch(`${element.href}`, {validate: true}).then((response) => {
+              console.log(response.url.magenta, response.status.red, response.statusText.red);        
+            });
+          });
+        };
       });
     }
-  }
+  });
 });
-
-//  Leer contenido del archivo
-//     fs.readFile(files, 'utf8', (err, data) => {
-//       if (err) {
-//         console.log(error);
-//       } else {
-//         mdLinks(data).forEach(element => {
-//           console.log(file, element.href, element.text);
-//         });
-//       };
-//     });
-//   }
-// };
-// });
 
      
