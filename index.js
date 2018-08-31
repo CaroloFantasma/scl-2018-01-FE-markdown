f30848de88bc8fc717da7e39fbc19c315cfc47d8
Carolina Lastra <carolastra.cofre@gmail.com>
2018-08-31 10:27:23 -0300

    Funcional nuevamente

--- index.js
+++ index.js
@@ -1,54 +1,10 @@
 #!/usr/bin/env node
+
 const fs = require('fs');
 const path = require('path');
 const Marked = require('marked');
 const fetch = require('node-fetch');
-const colors = require('colors');
-const mdLinks = require('./lib/md-links');
+const mdLinks = require('./md.js');
+const [,, ...args] = process.argv;
 
-// let [,, ...args] = process.argv;
-
-// mdLinks.validateFile(args[0]);
-
-// // Ruta actual del directorio (Current Working Directory)
-// let currentDirectory = process.cwd();
-// let cwdToString = Buffer.from(currentDirectory);
-// console.log(`Current working directory: ${process.cwd()}`.magenta);
-
-// //  Lee los contenidos del directorio
-// fs.readdir(cwdToString, (error, files) => {
-//   // Selecciona los archivos con extensión .md
-//   files.forEach(file => {
-//     if (path.extname(file).toLowerCase() === '.md') {
-//       // Leer contenido del archivo
-//       fs.readFile(file, 'utf8', (err, data) => {
-//         if (err) {
-//           console.log(error);
-//         } else {
-//           mdLinks(data).forEach(element => {
-//             console.log('Archivo:' + file.cyan, 'Link:' + element.href.green, 'Texto:' + element.text.blue);
-//             fetch(`${element.href}`, {validate: true}).then((response) => {
-//               console.log(response.url.magenta, response.status, response.statusText.red);
-//             });
-//           });
-//         };
-//       });
-//     }
-//   });
-// });
-
-// function readingFile(path) {
-//   return new Promise((resolve, reject) => {
-//     fs.readFile(path, 'utf-8', (error, data) => {
-//       if (error) {
-//         return reject(error);
-//       }
-//     });
-//   });
-// }
-
-// fs.readFile(file, function(err, contents) {  
-//   // fs.readFile(file, 'utf8', callbacalso be used  
-//   var lines = contents.toString().spli.length - 1;  
-//   console.log(lines);  
-
+mdLinks();
---
+++ md.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const Marked = require('marked');
+const fetch = require('node-fetch');
+const colors = require('colors');
+const linksMd = require('./lib/md-links');
+const [, , ...args] = process.argv;
+const options = require('./index');
+
+// mdLinks.validateFile(args[0]);
+let currentDirectory = process.cwd();
+let cwdToString = Buffer.from(currentDirectory);
+console.log(`Current working directory: ${process.cwd()}`.magenta);
+
+// Ruta actual del directorio (Current Working Directory)
+const mdLinks = function fileReadingPromise() {
+  fs.readdir(cwdToString, (error, files) => {
+    let currentDirectory = process.cwd();
+    let cwdToString = Buffer.from(currentDirectory);
+    options.validate = process.argv[3];
+    console.log(`Current working directory: ${process.cwd()}`.magenta);
+    console.log('process.argv:' + JSON.stringify(process.argv));
+
+
+    //  Lee los contenidos del directorio
+    fs.readdir(cwdToString, (error, files) => {
+      // Selecciona los archivos con extensión .md
+      files.forEach(file => {
+        if (path.extname(file).toLowerCase() === '.md') {
+          // Leer contenido del archivo
+          fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+              console.log(error);
+            } else {
+              let getLine = (data).split('\n').map((element, index) => linksMd(element, index + 1));
+              getLineNumber = getLine.filter(element => element.length !== 0);
+              let lineNumber = getLineNumber.reduce((firstValue, secondValue) => firstValue.concat(secondValue));
+              lineNumber.forEach(element => {
+                if (options.validate === '--validate') {
+                  fetch(`${element.href}`).then((response) => {
+                    console.log(response.url.magenta, response.status, response.statusText.red);
+                  }).catch((err) => {
+                    console.error('link no encontrado ' + err);
+                  });
+                } else {
+                  console.log('Archivo:' + file.cyan, 'Link:' + element.href.green, 'Texto:' + element.text.blue);
+                };
+              });
+            };
+          });
+        };
+      });
+    });
+  });
+};
+
+module.exports = mdLinks;
